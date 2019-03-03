67f9e456038530adb01e79571c69f465e6438f77
Francis Rodríguez <frc13elche@gmail.com>
2019-03-03 19:01:38 +0100

    refactorizado

--- videoJuego/src/net/instituto/desa/Game.java
+++ videoJuego/src/net/instituto/desa/Game.java
@@ -1,20 +1,24 @@
 package net.instituto.desa;
+import java.util.*;
 
 public class Game {
-	Jugador j;
-
-	public void movimiento(String m) {
-		if (m.equalsIgnoreCase("Derecha")) {
-			j.setX(j.getX() + 1);
-		}
-		if (m.equalsIgnoreCase("Izquierda")) {
-			j.setX(j.getX() - 1);
-		}
-		if (m.equalsIgnoreCase("Arriba")) {
-			j.setY(j.getY() - 1);
-		}
-		if (m.equalsIgnoreCase("Abajo")) {
-			j.setY(j.getY() + 1);
-		}
+	static final String ABAJO = "Abajo";
+	static final String ARRIBA = "Arriba";
+	static final String IZQUIERDA = "Izquierda";
+	static final String DERECHA = "Derecha";
+	
+	public static void main(String[] args) {
+	
+	Jugador j = new Jugador();
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.println("Introduce el movimiento...");
+	
+	String a = sc.nextLine();
+	
+	j.movimiento(a);
+	
+	System.out.println("Posición actual x: "+ j.getX() + ",y: " + j.getY() );
+	
 	}
 }
